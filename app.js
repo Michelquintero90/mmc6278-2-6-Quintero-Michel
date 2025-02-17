7a17017c3f45556ac5035f7c0afcf913bd811bc8
michelq <michel.quintero24@gmail.com>
2025-02-16 21:02:05 -0500

    Upload archives

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const apiRoutes = require('./routes/api-routes')
+const htmlRoutes = require('./routes/html-routes')
+const exphbs = require('express-handlebars')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.use('/', htmlRoutes)
+app.use('/api', apiRoutes)
+
+module.exports = app
---
+++ routes/html-routes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router()
+const db = require('../db')
+
+router.get('/', async (req, res) => {
+  const [rows] = await db.query('SELECT * FROM inventory;')
+  const [[{cartCount}]] = await db.query('SELECT SUM(quantity) AS cartCount FROM cart;')
+
+  res.render('index', { products: rows, cartCount })
+})
+
+router.get('/product/:id', async (req, res) => {
+  const [[product]] = await db.query(
+    'SELECT * FROM inventory WHERE id=?;',
+    [req.params.id]
+  )
+  const [[{cartCount}]] = await db.query('SELECT SUM(quantity) AS cartCount FROM cart;')
+
+  res.render('product', { product, cartCount })
+})
+
+router.get('/cart', async (req, res) => {
+  const [cartItems] = await db.query(
+    `SELECT
+        cart.id,
+        cart.inventory_id AS inventoryId,
+        cart.quantity,
+        inventory.price,
+        ROUND(inventory.price * cart.quantity, 2) AS calculatedPrice,
+        inventory.name,
+        inventory.image,
+        inventory.quantity AS inventoryQuantity
+      FROM cart LEFT JOIN inventory ON cart.inventory_id=inventory.id`
+  )
+  const total = cartItems
+    .reduce((total, item) => item.calculatedPrice + total, 0)
+    .toFixed(2)
+
+  res.render('cart', { cartItems, total })
+})
+
+module.exports = router
