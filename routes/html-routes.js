f32dece6fe120af45626ff995a1417131bbf74c4
michelq <michel.quintero24@gmail.com>
2025-02-17 14:28:31 -0500

    Update Files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,29 +1,37 @@
 const router = require('express').Router()
 const db = require('../db')
-const path = require('path')
 
 router.get('/', async (req, res) => {
-  const [rows] = await db.query('SELECT * FROM inventory;')
-  const [[{cartCount}]] = await db.query('SELECT SUM(quantity) AS cartCount FROM cart;')
+  try {
+    const [rows] = await db.query('SELECT * FROM inventory;')
+    const [[{ cartCount }]] = await db.query('SELECT SUM(quantity) AS cartCount FROM cart;')
 
-  // TODO: Convert the response below to render a handlebars template
-  res.sendFile(path.join(__dirname, '../views/index.html'))
+    res.render('index', { inventory: rows, cartCount })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Error loading index page')
+  }
 })
 
 router.get('/product/:id', async (req, res) => {
-  const [[product]] = await db.query(
-    'SELECT * FROM inventory WHERE id=?;',
-    [req.params.id]
-  )
-  const [[{cartCount}]] = await db.query('SELECT SUM(quantity) AS cartCount FROM cart;')
+  try {
+    const [[product]] = await db.query(
+      'SELECT * FROM inventory WHERE id=?;',
+      [req.params.id]
+    )
+    const [[{ cartCount }]] = await db.query('SELECT SUM(quantity) AS cartCount FROM cart;')
 
-  // TODO: Convert the response below to render a handlebars template
-  res.sendFile(path.join(__dirname, '../views/product.html'))
+    res.render('product', { product, cartCount })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Error loading product page')
+  }
 })
 
 router.get('/cart', async (req, res) => {
-  const [cartItems] = await db.query(
-    `SELECT
+  try {
+    const [cartItems] = await db.query(
+      `SELECT
         cart.id,
         cart.inventory_id AS inventoryId,
         cart.quantity,
@@ -33,15 +41,16 @@ router.get('/cart', async (req, res) => {
         inventory.image,
         inventory.quantity AS inventoryQuantity
       FROM cart LEFT JOIN inventory ON cart.inventory_id=inventory.id`
-  )
-  // We could get the cart total with a separate SQL query,
-  // but let's just use some JavaScript instead.
-  const total = cartItems
-    .reduce((total, item) => item.calculatedPrice + total, 0)
-    .toFixed(2)
+    )
+    const total = cartItems
+      .reduce((total, item) => item.calculatedPrice + total, 0)
+      .toFixed(2)
 
-  // TODO: Convert the response below to render a handlebars template
-  res.sendFile(path.join(__dirname, '../views/cart.html'))
+    res.render('cart', { cartItems, total })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Error loading cart page')
+  }
 })
 
 module.exports = router
